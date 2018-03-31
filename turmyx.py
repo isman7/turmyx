7ff0417aa25e854c5c2394e47543159262dc32b1
Ismael Benito <isman7@gmail.com>
2018-04-01 00:43:25 +0200

    First attempt to have subcommands.

--- turmyx.py
+++ turmyx.py
@@ -13,24 +13,31 @@ IMG_EDITOR_EXT = [
 ]
 
 
-@click.command()
-@click.argument('file', type=click.Path(exists=True), )
-def cli(file):
+@click.group(invoke_without_command=True)
+@click.argument('file',
+                type=click.Path(exists=True),
+                required=False,
+                )
+@click.option('--config', default=False, required=False)
+def cli(file, config):
     """Example script."""
     click.echo('This is turmyx! A script launcher for downloaded files to Termux. Enjoy!')
 
-    print(file)
+    if isinstance(file, str) and not config:
+        file_name = os.path.basename(file)
 
-    file_name = os.path.basename(file)
-
-    extension = file_name.split('.')[-1]
-    if extension in TEXT_EDITOR_EXT:
-        subprocess.check_call(['nano', file])
-        print("(^o^)丿 : Successfully edited with Termux.")
-    elif extension in IMG_EDITOR_EXT:
-        print("¯\_ツ_/¯ : Not implemented yet, soon!")
-    else:
-        print("¯\_ツ_/¯ : Extension not recognised.")
-
+        extension = file_name.split('.')[-1]
+        if extension in TEXT_EDITOR_EXT:
+            subprocess.check_call(['nano', file])
+            print("(^o^)丿 : Successfully edited with Termux.")
+        elif extension in IMG_EDITOR_EXT:
+            print("¯\_ツ_/¯ : Not implemented yet, soon!")
+        else:
+            print("¯\_ツ_/¯ : Extension not recognised.")
 
 
+
+
+@cli.command()
+def opener():
+    print("opener")
